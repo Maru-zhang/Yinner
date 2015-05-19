9c2d3652e7eb2dc77163ce85ad218dd0f32492cc
Maru-zhang <475435200@qq.com>
2015-05-19 13:00:34 +0800

    Initial Commit

---
+++ Yinner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yinner
+//
+//  Created by Maru on 15/5/19.
+//  Copyright (c) 2015年 Alloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
